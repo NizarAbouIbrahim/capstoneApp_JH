ecd8ba5bf5875361892c916c3531a09a61ccccf7
NizarAbouIbrahim <nizar.abouibrahim@lau.edu>
2023-04-02 18:21:31 +0300

    Initial version of capstoneApp_JH generated by generator-jhipster@7.9.3

---
+++ src/main/java/fyp/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package fyp.repository;
+
+import fyp.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
